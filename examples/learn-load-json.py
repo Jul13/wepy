db6adf8074a1615c799c338928cd0c170936ad8b
jreynier <julien.reynier@tobam.fr>
2017-04-20 02:15:05 +0200

    first commit

--- examples/learn-load-json.py
+++ examples/learn-load-json.py
@@ -1,5 +1,5 @@
 # ==============================================================================
-# title           : lean-matplotlib
+# title           : lean-load-json
 # description     :
 # author          : Julien Reynier
 # date            : 31/03/2017
@@ -8,9 +8,34 @@
 # ==============================================================================
 
 
-import wepy.config as cf
+import wepy.configuration  as cf
 import json
 import os
+import re
+# from pprint import pprint
+import dotmap
 
-path_trips = cf.
-os.
+# The file location that is in our config file
+path_trips = cf.PATHS.trips
+
+# Now find the files with answers
+list_files = os.listdir(path_trips)
+
+r = re.compile('response.txt')
+list_files = list(filter(r.search, list_files))
+len(list_files)
+
+# loads a particular file
+data_file = open(path_trips + list_files[0])
+# this builds a dictionnary from the json file
+data = json.load(data_file)
+
+# dotmap transform dictionaries into structure (easier completion)
+simpler = dotmap.DotMap(data)
+
+simpler.fuelEstimation.co2Emission
+data['fuelEstimation']['co2Emission']
+
+# pprint(data)
+
+# now data is a regular dictionary accessible like this
